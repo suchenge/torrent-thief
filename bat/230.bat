8efb6734f3e2f17f7b8cea2111397775ad5b1741
Vito <vito.su@live.com>
2018-06-10 23:50:56 +0800

    Update 230.bat

--- bat/230.bat
+++ bat/230.bat
@@ -1,4 +1,4 @@
 cd ..
 node tools\setup.js
-node --nouse-idle-notification --gc_global --max-old-space-size=2048 start.js 230 2018-01-27 null
+node --nouse-idle-notification --gc_global --max-old-space-size=2048 start.js 230 2018-04-22 null
 pause
